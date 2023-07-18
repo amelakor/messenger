7814cd5af8ad9ba94efb67130717a8247a7cedbc
amelakor <amela_korjenic@hotmail.com>
2023-07-18 10:51:51 +0200

    Active status logic

---
+++ pages/api/pusher/auth.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+
+import { pusherServer } from "@/app/libs/pusher";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+export default async function handler(
+    request: NextApiRequest,
+    response: NextApiResponse
+) {
+    const session = await getServerSession({ req: request, ...authOptions });
+
+    if (!session?.user?.email) {
+        return response.status(401).json({ error: "Unauthorized" });
+    }
+
+    const socketid = request.body.socket_id;
+    const channel = request.body.channel_name;
+    const data = {
+        user_id: session.user.email,
+    };
+
+    const authResponse = pusherServer.authorizeChannel(socketid, channel, data);
+
+    return response.send(authResponse);
+}
